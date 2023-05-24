0e5e5dd56f798e41f6dbcd81765d34505de4bc88
Larry.Lai <Larry.Lai@portwell.com.tw>
2023-05-24 14:18:52 +0800

    create project

---
+++ Serilog_Library.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Serilog.Events;
+
+namespace Serilog.AspNetCore.Library
+{
+    static public class Logger
+    {
+        static public void UseSerilogAspNetCore(this WebApplicationBuilder builder)
+        {
+            builder.Host.UseSerilog((context, configuration) => configuration
+            .WriteTo.Console()
+            .WriteTo.File(
+            path: "logs/log-.log",
+            restrictedToMinimumLevel: LogEventLevel.Warning,
+
+            rollingInterval: RollingInterval.Day,
+            retainedFileCountLimit: 365
+            ));
+        }
+
+        static public void Verbose(string msg) => Log.Verbose(msg);
+        static public void Debug(string msg) => Log.Debug(msg);
+        static public void Information(string msg) => Log.Information(msg);
+        static public void Warning(string msg) => Log.Warning(msg);
+        static public void Error(string msg) => Log.Error(msg);
+        static public void Fatal(string msg) => Log.Fatal(msg);
+
+        static public void CloseAndFlush() => Log.CloseAndFlush();
+    }
+}
